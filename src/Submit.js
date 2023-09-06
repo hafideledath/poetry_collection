5be5e99a16b561d5fcc56388f33192216be56770
Hafid Eledath <hafideledath@gmail.com>
2023-09-06 18:35:09 +0400

    form update

--- src/Submit.js
+++ src/Submit.js
@@ -4,7 +4,7 @@ function Submit() {
     return (
         <div className="submit">
             <img className="submit-a-poem" src={SubmitPoem} alt="submit a poem/" />
-            <iframe title="form" width="600px" height="480px" src="https://forms.office.com/Pages/ResponsePage.aspx?id=AMmUbCCnXEKuE9AmGtoJtKpyDYLlhaBCi7xs7uTBZq5UMEVSS1BKVVE2UlZMWFlXS0pPNkQzN0xZQi4u&embed=true" style={{border: "none", maxWidth:"100%", maxHeight:"100vh"}}> </iframe>
+            <iframe width="600px" height="480px" src="https://forms.microsoft.com/Pages/ResponsePage.aspx?id=AMmUbCCnXEKuE9AmGtoJtKpyDYLlhaBCi7xs7uTBZq5UQzI4R0lMRVRMQlk4OVpORUtJNTNIVFBNMi4u&embed=true" frameborder="0" marginwidth="0" marginheight="0" style={{border: "none", maxWidth: "100%", maxHeight: "100vh"}} allowfullscreen webkitallowfullscreen mozallowfullscreen msallowfullscreen> </iframe>
         </div>
     );
 }
